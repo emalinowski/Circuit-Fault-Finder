4b7b04b0444d982bd0cd2a3bed94d94d4bc3168f
emalinowski <emalinowski@siu.edu>
2016-10-28 15:12:49 -0500

    {   "id": "1234",   "username": {     "encrypted_data": "MMXLuXOD/4RB/F0Z+Vh+1Vet+hyRNRBLKlUn94g8uI8=\n",     "iv": "e7U3zOA8knC504w124ffdA==\n",     "version": 1,     "cipher": "aes-256-cbc"   },   "password": {     "encrypted_data": "mmPttCX5RPPkcaXcTU/S/bIDhpZzWRSEKucImrP/R6o=\n",     "iv": "F/+E9RzKUDl7UnzIn0VgUw==\n",     "version": 1,     "cipher": "aes-256-cbc"   } }

---
+++ file0645f5acfa94450288a728dfa56ed28dbcfe7228.rb
@@ -0,0 +1,15 @@
+{
+  "id": "1234",
+  "username": {
+    "encrypted_data": "MMXLuXOD/4RB/F0Z+Vh+1Vet+hyRNRBLKlUn94g8uI8=\n",
+    "iv": "e7U3zOA8knC504w124ffdA==\n",
+    "version": 1,
+    "cipher": "aes-256-cbc"
+  },
+  "password": {
+    "encrypted_data": "mmPttCX5RPPkcaXcTU/S/bIDhpZzWRSEKucImrP/R6o=\n",
+    "iv": "F/+E9RzKUDl7UnzIn0VgUw==\n",
+    "version": 1,
+    "cipher": "aes-256-cbc"
+  }
+}
